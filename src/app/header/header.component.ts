e889642be2ca82971168383e075a1c6ed8d1e44e
Martin Miklovič <miklovic@netgrif.com>
2021-03-14 12:15:38 +0100

    feat(header): Add responsible header with icons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,13 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
+  @ViewChild("headerTabs")
+  private headerTabs: ElementRef;
 
 }
