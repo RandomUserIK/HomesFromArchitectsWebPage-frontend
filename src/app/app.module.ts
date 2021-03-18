f86387780f762b09566e30abf6df4959ee876633
Martin Miklovič <miklovic@netgrif.com>
2021-03-18 13:34:35 +0100

    refactor(Home): Refactor home section and root styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ImageCarouselComponent } from './home/image-carousel/image-carousel.component';
-import { HomeComponent } from './home/home.component';
-import { HomeBubbleDescriptionComponent } from './home/home-bubble-description/home-bubble-description.component';
-import { IconsDescriptionComponent } from './home/icons-description/icons-description.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HomeModule} from "./home/home.module";
+import {HttpClientModule} from "@angular/common/http";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ImageCarouselComponent,
-    HomeComponent,
-    HomeBubbleDescriptionComponent,
-    IconsDescriptionComponent
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HomeImageCarouselComponent} from "./home-image-carousel/home-image-carousel.component";
+import {HomeComponent} from "./home.component";
+import {HomeBubbleDescriptionComponent} from "./home-bubble-description/home-bubble-description.component";
+import {HomeIconsDescriptionComponent} from "./home-icons-description/home-icons-description.component";
+import {HomeInteriorDescriptionComponent} from "./home-interior-description/home-interior-description.component";
+import {HomeIndividualProjectDescriptionComponent} from "./home-individual-project-description/home-individual-project-description.component";
+import {InstagramSearchService} from "./services/instagram-search.service";
+import {youTubeSearchInjectables} from "./services/InstagramSearchInjectables";
+import { HomeInstagramGalleryComponent } from './home-instagram-gallery/home-instagram-gallery.component';
+import { HomeInstagramPostComponent } from './home-instagram-post/home-instagram-post.component';
+import { HomeContactComponent } from './home-contact/home-contact.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeImageCarouselComponent,
+    HomeComponent,
+    HomeBubbleDescriptionComponent,
+    HomeIconsDescriptionComponent,
+    HomeInteriorDescriptionComponent,
+    HomeIndividualProjectDescriptionComponent,
+    HomeInstagramGalleryComponent,
+    HomeInstagramPostComponent,
+    HomeContactComponent,
+  ],
+  exports: [
+    HomeComponent
+  ],
+  providers: [
+    InstagramSearchService,
+    youTubeSearchInjectables
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
