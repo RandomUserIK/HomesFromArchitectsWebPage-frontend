65ab34fd852ef3c7aa5ee9062165180d8b33a386
xkovac <xkovaci1@stuba.sk>
2021-04-17 23:11:49 +0200

    refactor(project-view):
    - remove project price info styles file

--- src/app/public-view/components/project/components/project-price-info/project-price-info.component.ts
+++ src/app/public-view/components/project/components/project-price-info/project-price-info.component.ts
@@ -2,8 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-project-price-info',
-  templateUrl: './project-price-info.component.html',
-  styleUrls: ['./project-price-info.component.scss']
+  templateUrl: './project-price-info.component.html'
 })
 export class ProjectPriceInfoComponent {
 }
