55b20783082de3391db36949d6dab30deee23da9
Lukáš Rezák <rezak@netgrif.com>
2021-04-28 12:34:49 +0200

    HFA-62 - Umožniť nahrávanie viacerých fotiek pre pôdor
    - refactor(Data): Change floor plan image to gallery

--- src/app/components/data-fields/services/image-compression.service.ts
+++ src/app/components/data-fields/services/image-compression.service.ts
@@ -8,17 +8,29 @@ export class ImageCompressionService {
   constructor(private imageCompressService: NgxImageCompressService) {
   }
 
-  public compressFile(image: any, selectedFile: File): Promise<CompressedFile> {
+  private static dataURItoBlob(dataURI: string) {
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([int8Array], {type: 'image/jpeg'});
+  }
+
+  public compressFile(image: string, selectedFile: File): Promise<CompressedFile> {
     return this.executeCompression(image, selectedFile).then(compressedValue => {
       const compressionResult: CompressedFile = {};
       compressionResult.compressionResult = compressedValue;
-      compressionResult.file = new File([compressedValue], selectedFile.name, {type: 'image/png'});
-      console.log(compressionResult)
+      compressionResult.file = new File(
+        [ImageCompressionService.dataURItoBlob(compressedValue.split(',')[1])],
+        selectedFile.name,
+        {type: 'image/jpeg'});
       return compressionResult;
     });
   }
 
-  private async executeCompression(image: any, selectedFile: File): Promise<string> {
+  private async executeCompression(image: string, selectedFile: File): Promise<string> {
     const orientation = await this.imageCompressService.getOrientation(selectedFile);
     return this.imageCompressService.compressFile(image, orientation, 50, 50);
   }
