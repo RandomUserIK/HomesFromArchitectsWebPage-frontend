e135ae674842dfe718da601d71bc1f7099657b5b
xkovac <xkovaci1@stuba.sk>
2021-05-04 10:14:23 +0200

    fix(sonarqube):
    - fix sonar code smells

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -2,8 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: []
+  templateUrl: './not-found.component.html'
 })
 export class NotFoundComponent implements OnInit {
 
