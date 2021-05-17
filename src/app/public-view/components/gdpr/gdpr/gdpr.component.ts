70eb9a1157579f25c8cc2b084aae0e195ee64512
Lukáš Rezák <rezak@netgrif.com>
2021-05-17 10:04:04 +0200

    refactor(Sonar): remove unused imports

--- src/app/public-view/components/gdpr/gdpr/gdpr.component.ts
+++ src/app/public-view/components/gdpr/gdpr/gdpr.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 import {GdprTextSection} from '../components/models/GdprTextSection';
 import data from 'src/app/public-view/components/gdpr/components/models/gdpr-data.json';
 
 @Component({
   selector: 'app-gdpr',
-  templateUrl: './gdpr.component.html',
-  styleUrls: ['./gdpr.component.scss']
+  templateUrl: './gdpr.component.html'
 })
 export class GdprComponent{
 
