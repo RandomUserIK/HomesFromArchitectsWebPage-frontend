4b5268d3cd9162e74e3847991ec82386ea2fc3c0
Lukáš Rezák <rezak@netgrif.com>
2021-05-23 18:33:31 +0200

    refactor(Instagram): refactor instagram service and components to fetch instagram data from backend

--- src/app/public-view/components/home/services/instagram-search.service.ts
+++ src/app/public-view/components/home/services/instagram-search.service.ts
@@ -1,29 +1,18 @@
-import {Inject, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {InstagramSearchResult} from '../models/instagram-search-result';
-
-export const INSTAGRAM_API_KEY =
-  'IGQVJXbEFLYWp5RjRJTTB2OTlLVUVYTWF3YmJlTmZArV1p5WUd1S3ZARcjNXX1IyWWp1eVFwVWdwNGtHY01PbHN3TGZAnZA0hVM0hGdlZAqWFd2eVY3UDI4b3ZA1c0YwbVhVbjVlR0ZAvRlJGQkJONWFfZAlNOUQZDZD';
-export const INSTAGRAM_API_URL =
-  'https://graph.instagram.com/me/media';
+import {environment} from '../../../../../environments/environment';
 
 
 @Injectable()
 export class InstagramSearchService {
 
-  constructor(private http: HttpClient,
-              @Inject(INSTAGRAM_API_KEY) private apiKey: string,
-              @Inject(INSTAGRAM_API_URL) private apiUrl: string) {
+  constructor(private http: HttpClient) {
   }
 
-  public search(): Observable<InstagramSearchResult> {
-    const params: string = [
-      `fields=id,caption,media_url`,
-      `access_token=${this.apiKey}`
-    ].join('&');
-
-    const queryUrl = `${this.apiUrl}?${params}`;
-    return this.http.get<InstagramSearchResult>(queryUrl).pipe();
+  public search(limit: number): Observable<InstagramSearchResult> {
+    const url = environment.providers.resources.find(resource => resource.name === 'instagram-endpoint').address + '?limit='+limit;
+    return this.http.get<InstagramSearchResult>(url).pipe();
   }
 }
