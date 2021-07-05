138c6db5140dbd51249d88aa0c55dd865bb3a0c3
b0tka <martinmiklovic10@gmail.com>
2021-07-05 18:43:27 +0200

    fix(Lint): Fix lint errors

--- src/app/animate/animate.component.ts
+++ src/app/animate/animate.component.ts
@@ -1,59 +1,77 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  HostBinding,
-  HostListener,
-  Input,
-  OnDestroy,
-  OnInit,
-  Output,
-  Renderer2
-} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostBinding, HostListener, Input, OnDestroy, OnInit, Output, Renderer2} from '@angular/core';
+import {beat, bounce, flip, headShake, heartBeat, jello, pulse, rubberBand, shake, swing, tada, wobble} from './attention-seekers';
 import {delay, distinctUntilChanged, map, startWith, takeWhile} from 'rxjs/operators';
 import {coerceBooleanProperty, coerceNumberProperty} from '@angular/cdk/coercion';
 import {Subject, Subscription} from 'rxjs';
 import {state, style, trigger} from '@angular/animations';
 import {AnimateService} from './animate.service';
-// Animations
-import {
-  beat,
-  bounce,
-  flip,
-  headShake,
-  heartBeat,
-  jello,
-  pulse,
-  rubberBand,
-  shake,
-  swing,
-  tada,
-  wobble
-} from './attention-seekers';
 import {bounceIn, bumpIn, fadeIn, flipIn, jackInTheBox, landing, rollIn, zoomIn} from './entrances';
 import {bounceOut, fadeOut, hinge, rollOut, zoomOut} from './exits';
 
-export type wmAnimateSpeed = 'slower'|'slow'|'normal'|'fast'|'faster';
+export type wmAnimateSpeed = 'slower' | 'slow' | 'normal' | 'fast' | 'faster';
 export type wmAnimations =
-  // Attention seekers
-  'beat'|'bounce'|'flip'|'headShake'|'heartBeat'|'jello'|'pulse'|'rubberBand'|'shake'|'swing'|'tada'|'wobble'|
-  // Entrances
-  'bumpIn'|'bounceIn'|'bounceInDown'|'bounceInLeft'|'bounceInUp'|'bounceInRight'|'fadeIn'|'fadeInRight'|'fadeInLeft'|'fadeInUp'|'fadeInDown'|'flipInX'|'flipInY'|'jackInTheBox'|'landing'|'rollIn'|'zoomIn'|'zoomInDown'|'zoomInLeft'|'zoomInUp'|'zoomInRight'|
-  // Exits
-  'bounceOut'|'bounceOutDown'|'bounceOutUp'|'bounceOutRight'|'bounceOutLeft'|'fadeOut'|'fadeOutRight'|'fadeOutLeft'|'fadeOutDown'|'fadeOutUp'|'hinge'|'rollOut'|'zoomOut'|'zoomOutDown'|'zoomOutRight'|'zoomOutUp'|'zoomOutLeft'|
-  // None
-  'none';
+  'beat'
+  | 'bounce'
+  | 'flip'
+  | 'headShake'
+  | 'heartBeat'
+  | 'jello'
+  | 'pulse'
+  | 'rubberBand'
+  | 'shake'
+  | 'swing'
+  | 'tada'
+  | 'wobble'
+  | 'bumpIn'
+  | 'bounceIn'
+  | 'bounceInDown'
+  | 'bounceInLeft'
+  | 'bounceInUp'
+  | 'bounceInRight'
+  | 'fadeIn'
+  | 'fadeInRight'
+  | 'fadeInLeft'
+  | 'fadeInUp'
+  | 'fadeInDown'
+  | 'flipInX'
+  | 'flipInY'
+  | 'jackInTheBox'
+  | 'landing'
+  | 'rollIn'
+  | 'zoomIn'
+  | 'zoomInDown'
+  | 'zoomInLeft'
+  | 'zoomInUp'
+  | 'zoomInRight'
+  | 'bounceOut'
+  | 'bounceOutDown'
+  | 'bounceOutUp'
+  | 'bounceOutRight'
+  | 'bounceOutLeft'
+  | 'fadeOut'
+  | 'fadeOutRight'
+  | 'fadeOutLeft'
+  | 'fadeOutDown'
+  | 'fadeOutUp'
+  | 'hinge'
+  | 'rollOut'
+  | 'zoomOut'
+  | 'zoomOutDown'
+  | 'zoomOutRight'
+  | 'zoomOutUp'
+  | 'zoomOutLeft'
+  | 'none';
 
 @Component({
   selector: '[wmAnimate]',
   template: '<ng-content></ng-content>',
-  animations: [ trigger('animate', [
+  animations: [trigger('animate', [
     // Attention seekers
-    ...beat,...bounce,...flip,...headShake,...heartBeat,...jello,...pulse,...rubberBand,...shake,...swing,...tada,...wobble,
+    ...beat, ...bounce, ...flip, ...headShake, ...heartBeat, ...jello, ...pulse, ...rubberBand, ...shake, ...swing, ...tada, ...wobble,
     // Entrances
-    ...bumpIn,...bounceIn,...fadeIn,...flipIn,...jackInTheBox,...landing,...rollIn,...zoomIn,
+    ...bumpIn, ...bounceIn, ...fadeIn, ...flipIn, ...jackInTheBox, ...landing, ...rollIn, ...zoomIn,
     // Exits
-    ...bounceOut,...fadeOut,...hinge,...rollOut,...zoomOut,
+    ...bounceOut, ...fadeOut, ...hinge, ...rollOut, ...zoomOut,
     // None
     state('none', style('*')), state('idle-none', style('*'))
   ])]
@@ -62,33 +80,41 @@ export class AnimateComponent implements OnInit, OnDestroy {
 
   private replay$ = new Subject<boolean>();
   private sub: Subscription;
-
-  // Animating parameters
   private timing: string;
   private delay: string;
-
-  // Animating properties
+  private threshold = 0;
   public animating = false;
   public animated = false;
+  public paused = false;
+  public once = false;
 
-  constructor(private elm: ElementRef, private scroll: AnimateService, private renderer: Renderer2) {}
+
+  constructor(private elm: ElementRef, private scroll: AnimateService, private renderer: Renderer2) {
+  }
 
   @HostBinding('@animate')
   public trigger;
 
-  private get idle() { return { value: `idle-${this.animate}` }; }
+  private get idle() {
+    return {value: `idle-${this.wmAnimate}`};
+  }
+
   private get play() {
 
-    const params = {};
+    const params: { timing?: string, delay?: string } = {};
     // Builds the params object, so, leaving to the default values when undefined
-    if(!!this.timing) { params['timing'] = this.timing; }
-    if(!!this.delay) { params['delay'] = this.delay; }
+    if (!!this.timing) {
+      params.timing = this.timing;
+    }
+    if (!!this.delay) {
+      params.delay = this.delay;
+    }
 
-    return { value: this.animate, params };
+    return {value: this.wmAnimate, params};
   }
 
   /** Selects the animation to be played */
-  @Input('wmAnimate') animate: wmAnimations;
+  @Input() wmAnimate: wmAnimations;
 
   /** Speeds up or slows down the animation */
   @Input() set speed(speed: wmAnimateSpeed) {
@@ -103,62 +129,78 @@ export class AnimateComponent implements OnInit, OnDestroy {
   }
 
   /** Delays the animation */
-  @Input('delay') set postpone(delay: string) {
+  @Input('delay') set postpone(delayInput: string) {
     // Coerces the input into a number first
     const value = coerceNumberProperty(delay, 0);
-    if(value) {
+    if (value) {
       // Turns a valid number into a ms delay
       this.delay = `${value}ms`;
-    }
-    else {
+    } else {
       // Test the string for a valid delay combination
-      this.delay = /^\d+(?:ms|s)$/.test(delay) ? delay : '';
+      this.delay = /^\d+(?:ms|s)$/.test(delayInput) ? delayInput : '';
     }
   }
 
   /** Disables the animation */
-  @Input('disabled') set disableAnimation(value: boolean) { this.disabled = coerceBooleanProperty(value); }
+  @Input('disabled') set disableAnimation(value: boolean) {
+    this.disabled = coerceBooleanProperty(value);
+  }
+
   @HostBinding('@.disabled')
   public disabled = false;
 
   /** Emits at the end of the animation */
-  @Output() start = new EventEmitter<void>();
-  @HostListener('@animate.start')
-  public animationStart() { this.animating = true; this.animated = false; this.start.emit(); }
+  @Output() startEmitter = new EventEmitter<void>();
 
   /** Emits at the end of the animation */
-  @Output() done = new EventEmitter<void>();
-  @HostListener('@animate.done')
+  @Output() doneEmitter = new EventEmitter<void>();
+
+  @HostListener('@animate.start')
+  public animationStart() {
+    this.animating = true;
+    this.animated = false;
+    this.startEmitter.emit();
+  }
+
+  @HostListener('@animate.doneEmitter')
   public animationDone() {
 
-    this.animating = false; this.animated = true; this.done.emit();
+    this.animating = false;
+    this.animated = true;
+    this.doneEmitter.emit();
 
     /**
      * Removes spurious 'animation' style from the element once done with the animation.
      * This behaviour has been observed when running on iOS devices where for some reason
      * the animation engine do not properly clean-up the animation style using cubic-bezier()
      * as its timing function. The issue do not appear with ease-in/out and others.
-     * */
+     */
     this.renderer.removeStyle(this.elm.nativeElement, 'animation');
   }
 
-  /** When true, keeps the animation idle until the next replay triggers */
-  @Input('paused') set pauseAnimation(value: boolean) { this.paused = coerceBooleanProperty(value); }
-  public paused: boolean = false;
 
-  /** When defined, triggers the animation on element scrolling in the viewport by the specified amount. Amount defaults to 50% when not specified */
-  @Input('aos') set enableAOS(value: number) { this.threshold = coerceNumberProperty(value, 0.5); }
-  private threshold: number = 0;
+  /** When true, keeps the animation idle until the next replay triggers */
+  @Input('paused') set pauseAnimation(value: boolean) {
+    this.paused = coerceBooleanProperty(value);
+  }
+
+  /** When defined, triggers the animation on element scrolling in the viewport by the specified amount.
+   *  Amount defaults to 50% when not specified
+   */
+  @Input('aos') set enableAOS(value: number) {
+    this.threshold = coerceNumberProperty(value, 0.5);
+  }
 
   /** When true, triggers the animation on element scrolling in the viewport */
-  @Input('once') set aosOnce(value: boolean) { this.once = coerceBooleanProperty(value); }
-  public once: boolean = false;
+  @Input('once') set aosOnce(value: boolean) {
+    this.once = coerceBooleanProperty(value);
+  }
 
   /** Replays the animation */
   @Input() set replay(replay: any) {
 
     // Re-triggers the animation again on request (skipping the very fist value)
-    if(!!this.trigger && coerceBooleanProperty(replay)) {
+    if (!!this.trigger && coerceBooleanProperty(replay)) {
 
       this.trigger = this.idle;
       this.replay$.next(true);
@@ -169,7 +211,6 @@ export class AnimateComponent implements OnInit, OnDestroy {
 
     // Triggers the animation based on the input flags
     this.sub = this.replay$.pipe(
-
       // Waits the next round to re-trigger
       delay(0),
 
@@ -180,10 +221,10 @@ export class AnimateComponent implements OnInit, OnDestroy {
       this.scroll.trigger(this.elm, this.threshold),
 
       // Stop taking the first on trigger when aosOnce is set
-      takeWhile(trigger => !trigger || !this.once, true),
+      takeWhile(triggerInput => !triggerInput || !this.once, true),
 
       // Maps the trigger into animation states
-      map( trigger => trigger ? this.play : this.idle ),
+      map(triggerInput => triggerInput ? this.play : this.idle),
 
       // Always start with idle
       startWith(this.idle),
@@ -192,9 +233,11 @@ export class AnimateComponent implements OnInit, OnDestroy {
       distinctUntilChanged(),
 
       // Triggers the animation to play or to idle
-    ).subscribe( trigger => this.trigger = trigger );
+    ).subscribe(triggerInput => this.trigger = triggerInput);
   }
 
   // Disposes of the observable
-  ngOnDestroy() { this.sub.unsubscribe(); }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
